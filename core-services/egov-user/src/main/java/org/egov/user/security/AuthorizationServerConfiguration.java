8097ab57f3c745a9b81dcc1d7c460bfcba0196b9
Raju <raju@sparrowsoftech.net>
2026-04-18 13:09:14 +0530

    updated code to check data in http cookies to resolve audit issue

--- core-services/egov-user/src/main/java/org/egov/user/security/AuthorizationServerConfiguration.java
+++ core-services/egov-user/src/main/java/org/egov/user/security/AuthorizationServerConfiguration.java
@@ -1,5 +1,9 @@
 package org.egov.user.security;
 
+import static org.egov.user.config.UserServiceConstants.USER_CLIENT_ID;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.egov.user.security.oauth2.custom.CustomTokenEnhancer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -7,74 +11,110 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import redis.clients.jedis.JedisShardInfo;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
-import static org.egov.user.config.UserServiceConstants.USER_CLIENT_ID;
+import redis.clients.jedis.JedisShardInfo;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
 
-    @Value("${spring.redis.host}")
-    private String host;
+	@Value("${spring.redis.host}")
+	private String host;
 
-    @Value("${access.token.validity.in.minutes}")
-    private int accessTokenValidityInMinutes;
+	@Value("${access.token.validity.in.minutes}")
+	private int accessTokenValidityInMinutes;
 
-    @Value("${refresh.token.validity.in.minutes}")
-    private int refreshTokenValidityInMinutes;
+	@Value("${refresh.token.validity.in.minutes}")
+	private int refreshTokenValidityInMinutes;
 
-    @Autowired
-    private AuthenticationManager customAuthenticationManager;
+	@Autowired
+	private AuthenticationManager customAuthenticationManager;
 
-    @Autowired
-    private CustomTokenEnhancer customTokenEnhancer;
+	@Autowired
+	private CustomTokenEnhancer customTokenEnhancer;
 
-    @Autowired
-    private ClientDetailsService clientDetailsService;
+	@Autowired
+	private ClientDetailsService clientDetailsService;
 
-    @Autowired
-    private TokenStore tokenStore;
+	@Autowired
+	private TokenStore tokenStore;
 
-    @Override
-    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        final int accessTokenValidityInSeconds = accessTokenValidityInMinutes * 60;
-        final int refreshTokenValidityInSeconds = refreshTokenValidityInMinutes * 60;
-        clients.inMemory().withClient(USER_CLIENT_ID)
-                .authorizedGrantTypes("authorization_code", "refresh_token", "password")
-                .authorities("ROLE_APP", "ROLE_CITIZEN", "ROLE_ADMIN", "ROLE_EMPLOYEE").scopes("read", "write")
-                .refreshTokenValiditySeconds(refreshTokenValidityInSeconds)
-                .accessTokenValiditySeconds(accessTokenValidityInSeconds);
-    }
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		final int accessTokenValidityInSeconds = accessTokenValidityInMinutes * 60;
+		final int refreshTokenValidityInSeconds = refreshTokenValidityInMinutes * 60;
+		clients.inMemory().withClient(USER_CLIENT_ID)
+				.authorizedGrantTypes("authorization_code", "refresh_token", "password")
+				.authorities("ROLE_APP", "ROLE_CITIZEN", "ROLE_ADMIN", "ROLE_EMPLOYEE").scopes("read", "write")
+				.refreshTokenValiditySeconds(refreshTokenValidityInSeconds)
+				.accessTokenValiditySeconds(accessTokenValidityInSeconds);
+	}
 
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.tokenServices(customTokenServices()).authenticationManager(customAuthenticationManager);
+	}
 
-    @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.tokenServices(customTokenServices())
-                .authenticationManager(customAuthenticationManager);
-    }
+	@Bean
+	public JedisConnectionFactory connectionFactory() throws Exception {
+		return new JedisConnectionFactory(new JedisShardInfo(host));
+	}
 
-    @Bean
-    public JedisConnectionFactory connectionFactory() throws Exception {
-        return new JedisConnectionFactory(new JedisShardInfo(host));
-    }
+//    @Bean
+//    public DefaultTokenServices customTokenServices() {
+//        DefaultTokenServices tokenServices = new DefaultTokenServices();
+//        tokenServices.setTokenEnhancer(customTokenEnhancer);
+//        tokenServices.setTokenStore(tokenStore);
+//        tokenServices.setSupportRefreshToken(true);
+//        tokenServices.setReuseRefreshToken(true);
+//        tokenServices.setAuthenticationManager(customAuthenticationManager);
+//        tokenServices.setClientDetailsService(clientDetailsService);
+//        return tokenServices;
+//    }
+	
+	@Bean
+	public DefaultTokenServices customTokenServices() {
+	    return new DefaultTokenServices() {
 
-    @Bean
-    public DefaultTokenServices customTokenServices() {
-        DefaultTokenServices tokenServices = new DefaultTokenServices();
-        tokenServices.setTokenEnhancer(customTokenEnhancer);
-        tokenServices.setTokenStore(tokenStore);
-        tokenServices.setSupportRefreshToken(true);
-        tokenServices.setReuseRefreshToken(true);
-        tokenServices.setAuthenticationManager(customAuthenticationManager);
-        tokenServices.setClientDetailsService(clientDetailsService);
-        return tokenServices;
-    }
+	        @Override
+	        public OAuth2AccessToken createAccessToken(OAuth2Authentication authentication) {
+
+	            OAuth2AccessToken token = super.createAccessToken(authentication);
+
+	            HttpServletResponse response =
+	                ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getResponse();
+
+	            boolean isSecure = true; // 👉 change to false for localhost testing
+
+	            // 🔐 Access Token Cookie
+	            response.addHeader("Set-Cookie",
+	                "accessToken=" + token.getValue() +
+	                "; HttpOnly; Path=/; SameSite=Strict" +
+	                (isSecure ? "; Secure" : "") +
+	                "; Max-Age=" + (accessTokenValidityInMinutes * 60));
+
+	            // 🔐 Refresh Token Cookie
+	            response.addHeader("Set-Cookie",
+	                "refreshToken=" + token.getRefreshToken().getValue() +
+	                "; HttpOnly; Path=/; SameSite=Strict" +
+	                (isSecure ? "; Secure" : "") +
+	                "; Max-Age=" + (refreshTokenValidityInMinutes * 60));
+
+	            return token;
+	        }
+	    };
+	}
+
+	
 }
